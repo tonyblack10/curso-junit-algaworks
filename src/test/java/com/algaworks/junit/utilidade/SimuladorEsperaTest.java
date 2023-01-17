4a43f249863288f55d9283b84fb9863931776dce
Tony Costa <tonyblack10@gmail.com>
2023-01-16 22:56:21 -0300

    Asserções de Timeout

---
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -0,0 +1,16 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
+class SimuladorEsperaTest {
+
+    @Test
+    void deveEsperarENaoDarTimeout() {
+//        assertTimeout(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofSeconds(10)));
+        assertTimeoutPreemptively(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofMillis(10)));
+    }
+}
