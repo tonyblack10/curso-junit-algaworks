2f33b325682940064012aeb9c912e7ac1a5e319e
Tony Costa <tonyblack10@gmail.com>
2023-01-16 22:47:38 -0300

    Testando exceptions

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -13,4 +13,16 @@ class SaudacaoUtilTest {
         assertEquals("Bom dia", saudacao);
     }
 
+    @Test
+    public void deveLancarException() {
+        IllegalArgumentException illegalArgumentException =
+                assertThrows(IllegalArgumentException.class, () -> SaudacaoUtil.saudar(-10));
+
+        assertEquals("Hora inválida", illegalArgumentException.getMessage());
+    }
+
+    @Test
+    public void naoDeveLancarException() {
+        assertDoesNotThrow(() -> SaudacaoUtil.saudar(0));
+    }
 }
